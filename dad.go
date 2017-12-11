ddc0a1801ae1e6190b2e87172266c123fe93bf06
Alec West <anwest42@students.tntech.edu>
2017-12-10 21:50:52 -0600

    Create new dadBot struct for containing config

--- dad.go
+++ dad.go
@@ -1,7 +1,7 @@
 package main
 
-// TODO implement stuff that modifies conf file (grounding)
 // TODO move last reply to config
+// TODO dad, say is broken
 // TODO refactor stuff that manages the config (make a config navigation struct or something)
 // TODO add attribute for responses that involve reuse (ReuseContent bool)
 // TODO replace [...] blocks with %s and put them in a separate attribute (Format string)
@@ -51,32 +51,39 @@ type Reply struct {
 	Sent 		time.Time
 }
 
-var conf = initConfig()
-var serv = flag.String("server", conf.Ip + ":6667", "hostname and port for irc server to connect to")
-var nick = flag.String("nick", conf.DadName, "nickname for the bot")
-var lastReply Reply
+type DadBot struct {
+	Dad 		*hbot.Bot
+	Conf 		Configuration
+	LastReply 	Reply
+}
+
+var dbot DadBot
 
 func main() {
 	rand.Seed(time.Now().Unix())
 	flag.Parse()
+	dbot.Conf = initConfig()
+	serv := flag.String("server", dbot.Conf.Ip + ":6667", "hostname and port for irc server to connect to")
+	nick := flag.String("nick", dbot.Conf.DadName, "nickname for the bot")
 
 	hijackSession := func(bot *hbot.Bot) {
 		bot.HijackSession = true
 	}
 	channels := func(bot *hbot.Bot) {
-		bot.Channels = conf.Channels
+		bot.Channels = dbot.Conf.Channels
 	}
-	irc, err := hbot.NewBot(*serv, *nick, hijackSession, channels)
+	bot, err := hbot.NewBot(*serv, *nick, hijackSession, channels)
+	dbot.Dad = bot
 	if err != nil {
 		panic(err)
 	}
 
-	irc.AddTrigger(UserTrigger)
-	irc.AddTrigger(AdminTrigger)
-	irc.Logger.SetHandler(log.StdoutHandler)
+	dbot.Dad.AddTrigger(UserTrigger)
+	dbot.Dad.AddTrigger(AdminTrigger)
+	dbot.Dad.Logger.SetHandler(log.StdoutHandler)
 
 	// Start up bot (this blocks until we disconnect)
-	irc.Run()
+	dbot.Dad.Run()
 	fmt.Println("Bot shutting down.")
 }
 
@@ -93,12 +100,24 @@ func initConfig () Configuration {
 	return conf
 }
 
-func updateConfig (conf Configuration) {
-	jsonData, err := json.MarshalIndent(conf, "", "    ")
+func updateConfig () {
+	jsonData, err := json.MarshalIndent(dbot.Conf, "", "    ")
 	if (err != nil) {
 		panic(err)
 	}
 	ioutil.WriteFile("conf.json", jsonData, 0644)
+}
+
+func updateGrounding (content string, command string) {
+	i := stringInSlice(content, dbot.Conf.Grounded)
+
+	// log.Debug(fmt.Sprintf("index: %d, grounding/ungrounding: %s", i, content))
+	if (command == "[ground]" && i == -1) {
+		dbot.Conf.Grounded = append(dbot.Conf.Grounded, content)
+	} else if (command == "[unground]" && i != -1) {
+		dbot.Conf.Grounded[len(dbot.Conf.Grounded) - 1], dbot.Conf.Grounded[i] = dbot.Conf.Grounded[i], dbot.Conf.Grounded[len(dbot.Conf.Grounded) - 1]
+		dbot.Conf.Grounded = dbot.Conf.Grounded[:len(dbot.Conf.Grounded) - 1]
+	}
 }
 
 func testMessage (regex string, message *hbot.Message) bool {
@@ -106,7 +125,7 @@ func testMessage (regex string, message *hbot.Message) bool {
 	// err = errors.New("Forgot to include who the message was from")
 	r := regexp.MustCompile(regex)
 	if (r.MatchString(message.Content) && 
-		!stringInSlice(message.From, conf.Grounded) &&
+		stringInSlice(message.From, dbot.Conf.Grounded) == -1 &&
 		messageRate(message)) {
 		match = true
 	}
@@ -114,16 +133,16 @@ func testMessage (regex string, message *hbot.Message) bool {
 }
 
 func messageRate (m *hbot.Message) bool {
-	return (time.Since(lastReply.Sent) > (time.Duration(conf.MessageRate) * time.Second) || m.From == conf.Admin)
+	return (time.Since(dbot.LastReply.Sent) > (time.Duration(dbot.Conf.MessageRate) * time.Second) || m.From == dbot.Conf.Admin)
 }
 
-func stringInSlice (a string, s []string) bool {
-	for _, b := range s {
+func stringInSlice (a string, s []string) int {
+	for i, b := range s {
 		if (a == b) {
-			return true
+			return i
 		}
 	}
-	return false
+	return -1
 }
 
 func removeRegex (s string, regex string) string {
@@ -145,13 +164,13 @@ func setRecipient (m *hbot.Message, s SpeakData) string {
 	return to
 }
 
-func formatReply (m *hbot.Message, conf Configuration, admin_speak bool, s_index int) Reply {
+func formatReply (m *hbot.Message, admin_speak bool, s_index int) Reply {
 	var s SpeakData
 	var reply Reply
 	if (admin_speak) {
-		s = conf.AdminSpeak[s_index]
+		s = dbot.Conf.AdminSpeak[s_index]
 	} else {
-		s = conf.Speak[s_index]
+		s = dbot.Conf.Speak[s_index]
 	}
 
 	// Choose random response from list of responses (mostly used for jokes)
@@ -169,12 +188,13 @@ func formatReply (m *hbot.Message, conf Configuration, admin_speak bool, s_index
 		response.Message = strings.Replace(response.Message, "[from]", m.From, -1)
 	}
 	if (strings.Contains(response.Message, "[grounded]")) {
-		response.Message = strings.Replace(response.Message, "[grounded]", strings.Join(conf.Grounded, ", "), -1)
+		response.Message = strings.Replace(response.Message, "[grounded]", strings.Join(dbot.Conf.Grounded, ", "), -1)
 	}
 
 	// Manages all responses that reuse any content from the original message
-	for _, replace := range ([]string {"[mock]", "[repeat]", "[user]"}) {
+	for _, replace := range ([]string {"[mock]", "[repeat]", "[ground]", "[unground]"}) {
 		if (strings.Contains(response.Message, replace)) {
+			
 			// Modify who the message is sent to if it includes "user:" before the command
 			if (replace == "[repeat]") {
 				to := setRecipient(m, s)
@@ -182,9 +202,15 @@ func formatReply (m *hbot.Message, conf Configuration, admin_speak bool, s_index
 					reply.To = to
 				}
 			} else {
+				// Remove the part that the regex matched to
 				m.Content = removeRegex(m.Content, s.Regex)
 			}
 
+			// Manage grounding/ungrounding
+			updateGrounding(m.Content, replace)
+
+
+			// Replace [...] element in the response with what remains in the Content of the message
 			nonWord := regexp.MustCompile("^\\W+$")
 			if (len(m.Content) == 0 || nonWord.MatchString(m.Content)) {
 				response.Message = "" // Delete response if m.Content is empty
@@ -194,28 +220,26 @@ func formatReply (m *hbot.Message, conf Configuration, admin_speak bool, s_index
 		}
 	}
 	if (response.Message != "") {
-		log.Debug(fmt.Sprintf("updating responses count to %d", response.Count + 1))
-		response.Count++
 		if (admin_speak) {
-			conf.AdminSpeak[s_index].Response[rand_index].Count++
+			dbot.Conf.AdminSpeak[s_index].Response[rand_index].Count++
 		} else {
-			conf.Speak[s_index].Response[rand_index].Count++
+			dbot.Conf.Speak[s_index].Response[rand_index].Count++
 		}
 	}
 	reply.Content = strings.Split(response.Message, "\n")
 	return reply
 }
 
-func performAction (irc *hbot.Bot, m *hbot.Message, conf Configuration, admin_speak bool) bool {
+func performAction (irc *hbot.Bot, m *hbot.Message, admin_speak bool) bool {
 	var speak []SpeakData
 	if (admin_speak) {
-		speak = conf.AdminSpeak
+		speak = dbot.Conf.AdminSpeak
 	} else {
-		speak = conf.Speak
+		speak = dbot.Conf.Speak
 	}
 	for i, s := range speak {
 		if (testMessage(s.Regex, m)) {
-			reply := formatReply(m, conf, admin_speak, i)
+			reply := formatReply(m, admin_speak, i)
 			reply.Sent = time.Now()
 			numSent := 0
 			for _, line := range reply.Content {
@@ -226,13 +250,13 @@ func performAction (irc *hbot.Bot, m *hbot.Message, conf Configuration, admin_sp
 				}
 				// Make sure there is a timeout between multiple lines in a reply
 				if (len(reply.Content) > 1 && numSent > 0) {
-					time.Sleep(time.Duration(conf.Timeout) * time.Second)
+					time.Sleep(time.Duration(dbot.Conf.Timeout) * time.Second)
 				}
 			}
 			if (numSent > 0) {
 				// Record last sent message
-				lastReply = reply
-				updateConfig(conf)
+				dbot.LastReply = reply
+				updateConfig()
 				return true
 			}
 			// If a regex statement passed but nothing was sent, 
@@ -245,23 +269,25 @@ func performAction (irc *hbot.Bot, m *hbot.Message, conf Configuration, admin_sp
 
 var UserTrigger = hbot.Trigger {
 	func (bot *hbot.Bot, m *hbot.Message) bool {
-		return (m.From != conf.Admin)
+		return (m.From != dbot.Conf.Admin)
 	},
 	func (irc *hbot.Bot, m *hbot.Message) bool {
-		performAction(irc, m, conf, false)
+		_ = performAction(irc, m, false)
+		updateConfig()
 		return false
 	},
 }
 
 var AdminTrigger = hbot.Trigger {
 	func (bot *hbot.Bot, m *hbot.Message) bool {
-		return (m.From == conf.Admin)
+		return (m.From == dbot.Conf.Admin)
 	},
 	func (irc *hbot.Bot, m *hbot.Message) bool {
-		responded := performAction(irc, m, conf, true)
+		responded := performAction(irc, m, true)
 		if (!responded) {
-			performAction(irc, m, conf, false)
+			_ = performAction(irc, m, false)
 		}
+		updateConfig()
 		return false
 	},
 }
